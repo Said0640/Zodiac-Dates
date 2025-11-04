5a712667b3c115e985ae59572331acc42c5ca91b
Said <saidaliyev770@gmail.com>
2025-11-03 19:01:54 -0800

    Add files via upload

---
+++ Zodiac_Dates.py
@@ -0,0 +1,29 @@
+Zodiac = (input("What is your Zodiac?"))
+
+if(Zodiac == "Aquarious"):
+    print("Jan 20 - Feb 18")
+if(Zodiac == "Capricorn"):
+    print("Dec 22-Jan 20")
+if(Zodiac == "Pisces"):
+    print("Feb 19-March 20")
+if(Zodiac == "Aries"):
+    print("March 20-April 20")
+if(Zodiac == "Taurus"):
+    print("April 20-21 May")
+if(Zodiac == "Gemini"):
+    print("May 21-June 21")
+if(Zodiac == "Cancer"):
+    print("June 21-July 23")
+if(Zodiac == "Leo"):
+    print("July 23-August 23")
+if(Zodiac == "Virgo"):
+    print("Aug 23-Sep 23")
+if(Zodiac == "Libra"):
+    print("Sep 23-Oct 23")
+if(Zodiac == "Scorpio"):
+    print("Oct 23-Nov 22")
+if(Zodiac == "Sagittarius"):
+    print("Nov 22-Dec 22")
+
+
+
